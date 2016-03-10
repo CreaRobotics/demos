610705e831f61138a0269f97ec4f6af1357dc52e
Raúl Pérula-Martínez <raules@gmail.com>
2016-03-10 10:14:59 +0100

    added sumo example code for robot 1 and 2

---
+++ sumo/sumo_r2/sumo_r2.ino
@@ -0,0 +1,93 @@
+/**
+ * @file follow_me.ino
+ * @brief Example code for a follow me mono-sensor using the ProtoCREA robot.
+ * @author Raul Perula-Martinez (raules[at]gmail.com)
+ * @copyright CREA Robotica Educativa
+ */
+
+#include <Servo.h>
+#include <Ultrasonic.h>
+
+// constants
+
+// US sensor pins
+int ECHO_PIN = 2;
+int TRIG_PIN = 3;
+
+// motor pins
+int L_MOTOR_PIN = 9;
+int R_MOTOR_PIN = 10;
+
+// globals
+
+Ultrasonic us(TRIG_PIN, ECHO_PIN);
+
+Servo l_servo, r_servo;
+
+int dist = 0;
+
+// functions
+
+void go_straight()
+{
+    l_servo.write(180);
+    r_servo.write(0);
+}
+
+void go_back()
+{
+    l_servo.write(0);
+    r_servo.write(180);
+}
+
+void turn_left()
+{
+    l_servo.write(90);
+    r_servo.write(30);
+}
+
+void go_stop()
+{
+    l_servo.write(96);
+    r_servo.write(90);
+}
+
+/**
+ * @brief Initialization.
+ */
+void setup()
+{
+    // open serial port to 9600 bps
+    Serial.begin(9600);
+
+    // empty serial port
+    Serial.flush();
+
+    l_servo.attach(L_MOTOR_PIN);
+    r_servo.attach(R_MOTOR_PIN);
+}
+
+/**
+ * @brief Main loop of the program.
+ */
+void loop()
+{
+    // get time between pulses
+    long microsec = us.timing();
+
+    // get distances
+    dist = us.CalcDistance(microsec, Ultrasonic::CM);
+    
+    // control logic
+    if(dist <= 20) {
+        go_straight();
+        delay(500);
+    }
+    else {
+        turn_left();
+        delay(500);
+        go_stop();
+        delay(500);
+    }
+}
+
